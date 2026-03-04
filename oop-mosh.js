0a74773d7605e5fff71396bc82e25c318db4c321
jkian <slyrics443@gmail.com>
2026-03-05 00:30:24 +0800

    work but i dont think its the correct answer i shortcut somehow

---
+++ oop-mosh.js
@@ -0,0 +1,191 @@
+// // // // // // // yeah learning this is not just about the js anymore, its about oop
+
+// // // // // // const c = {
+// // // // // //   r: 1,
+// // // // // //   l: {
+// // // // // //     x: 2,
+// // // // // //     y: 1,
+// // // // // //   },
+// // // // // //   f: () => {
+// // // // // //     console.log(this.r);
+// // // // // //   },
+
+// // // // // //   a: function () {
+// // // // // //     console.log(this.r);
+// // // // // //   },
+// // // // // //   d: function () {
+// // // // // //     this.f();
+// // // // // //     // this is  becuase arrow function does not work well with object`)
+
+// // // // // //     this.a();
+// // // // // //     //  console.log(`this is ${a} prints 1 becuase it callable and can work with object`)
+// // // // // //   },
+// // // // // // };
+
+// // // // // // c.d();
+
+// // // // // //factory function
+// // // // // // function createC(rad){
+// // // // // //   return {
+// // // // // //   rad, //=> r:rad
+
+// // // // // //   f: () => {
+// // // // // //     console.log(this.rad);
+// // // // // //   },
+
+// // // // // //   d: function () {
+// // // // // //     this.f();
+// // // // // //   },
+// // // // // // }
+// // // // // // }
+
+// // // // // // const c =createC(4)
+// // // // // // console.log(c)
+
+// // // // // //constructer function
+// // // // // function Circle(r){
+// // // // //   console.log('this',this)
+// // // // //   this.r =r;
+// // // // //   this.draw=function(){
+// // // // //     console.log('draw');
+// // // // //   }
+// // // // // }
+
+// // // // // const a = new Circle(10)
+// // // // // console.log(a)
+
+// // // // // let x =10
+// // // // // let y =x
+
+// // // // // x=20
+
+// // // // // console.log(x,y)
+
+// // // // // let x =10
+// // // // // let y =x
+
+// // // // // x=20
+
+// // // // // console.log(x,y)
+
+// // // // // let x ={v:10}
+// // // // // function i(x){
+// // // // //   x.v++
+// // // // // }
+// // // // // console.log(i(x))
+// // // // // // console.log(i(x.v))
+// // // // //   // i think doing it like this does not work becuase x.v is 10 which is probable
+// // // // //   // considered as a primitative typeof, is of an object
+// // // // // console.log(x)
+// // // // // //knowing this types are important i just know this is were most bugs happend fuck
+
+// // // // function Circle(r){
+// // // //   console.log('this',this)
+// // // //   this.r =r;
+// // // //   this.draw=function(){
+// // // //     console.log('draw');
+// // // //   }
+// // // // }
+
+// // // // const a = new Circle(10)
+// // // // const h=a.location={x:1}
+// // // // console.log(h)
+
+// // // // const k='hola'
+// // // // console.log(a[k]={k:'hole'})
+// // // // // this is better than a.location becuase this is more robust and dynamic compate to a.location
+// // // // // also we can do think like that a[k] in a.k is not a think becuase a.k we are access the Object
+// // // // // not inputing value
+
+// // // function Circle(r){
+// // //   console.log('this',this)
+// // //   this.r =r;
+// // //   this.draw=function(){
+// // //     console.log('draw');
+// // //   }
+// // // }
+
+// // // const a = new Circle(10)
+
+// // // for(let x in a){
+// // //   if(typeof a[x] !== 'function')
+// // //       //check what is the type of a by going to a loop and checking thier type, if
+// // //     // it is a function it does not include it
+// // //   console.log(x,a[x])
+// // // }
+
+// // // const keyOnly=Object.keys(a)
+// // //   //get only the key
+// // // console.log(keyOnly)
+
+// // // if('r' in a){
+// // //   console.log('true')
+// // // }
+
+// // function Circle(r){
+// //   console.log('this',this)
+
+// //  let dl={x:0,y:0}
+// //   const op =function(c){
+// //     console.log('op')
+// //   }
+// //   ///this can nolonger be able to be able to access outside,a.op()
+
+// //   this.r =r;
+// //   this.draw=function(){
+// //     op(1)
+// //     console.log('draw');
+// //   }
+// // }
+
+// // const a = new Circle(10)
+// // console.log(a.draw())
+
+// // GEtter/Setter
+
+// function Circle(r) {
+//   console.log("this", this);
+
+//   let dl = { x: 0, y: 0 };
+//   // this.getdl= dl
+//   // this is not good because it can change the value a.getdl={k:2} { k: 2 }, if you
+//   // only want to show it done use this.getdl=dl becuase dl is an object
+
+//   this.getdl = function () {
+//     return dl;
+//   };
+
+//   this.r = r;
+//   this.draw = function () {
+//     op(1);
+//     console.log("draw");
+//   };
+
+//   Object.defineProperty(this, "getdl", {
+//     get: function () {
+//       //  this allows you to access getdl by a.getdl instead of a.getdl()
+//       // also this make sure you cannot modify 
+//       return dl
+//     },
+//     set:function(value){
+//       if(!value.x || !value.y)
+//         throw new Error('INvaldi');
+//       dl=value
+//     }
+//   });
+// }
+
+// const a = new Circle(10);
+// console.log(a.getdl);
+// console.log(a.getdl={l:19});//remove this becuase it breaks setter
+// console.log(a.dl={x:1});
+// console.log(a.getdl);
+
+// // output
+// // this Circle {}
+// // { x: 0, y: 0 }
+// // { l: 19 }
+// // { x: 0, y: 0 }
+
+
+
---
+++ stopwatch.js
@@ -0,0 +1,84 @@
+// create an oop stopwatch
+// feature
+//     -start time
+//     -stop time
+//     -duration (calculate the time between start and stop)
+//     -reset - reset time to zero
+// no ai you are allowed to search library for time
+
+const sw = function () {
+  let start, end;
+  this.startT = start;
+  this.endT = end;
+
+  this.startTime = function () {
+    this.startT = new Date();
+  };
+
+  this.endTime = function () {
+    this.endT = new Date();
+  };
+  this.duration = function () {
+    const el = (this.endT - this.startT) / 1000;
+    console.log(`Elapsed: ${el}s`);
+  };
+
+  this.reset = function () {
+    this.startT = start;
+  };
+
+  Object.defineProperty(this, "startTime", {
+    get: function () {
+      this.startTime();
+    },
+    set: function (value) {
+      if (true) throw new Error("Time has started");
+    },
+  });
+
+  Object.defineProperty(this, "endTime", {
+    get: function () {
+      this.endTime();
+    },
+    set: function (value) {
+      if (!value) throw new Error("Time has Ended");
+    },
+  });
+
+  Object.defineProperty(this, "duration", {
+    get: function () {
+      const el = (this.endT - this.startT) / 1000;
+      console.log(`Elapsed: ${el}s`);
+    },
+    set: function (value) {
+      if (!value) throw new Error("Time has has not been Started yet");
+    },
+  });
+
+  Object.defineProperty(this, "reset", {
+    get: function () {
+      return this.reset();
+    },
+  });
+};
+
+const s = new sw();
+/*
+
+
+ok this works now i just have to do the property thing
+so that i dont have to write it like a function with ()
+
+i was just overthinking it fuck, i though it was suppose to not be function
+pretty sure why let start=new Date and this.start= new Date was becuase it was 
+premititve type, which wont work becuase it does not change or can be modify
+because it is eqaul to new date it self, but shold it work because i start a new data then 
+staer a new one again with end, am confuse, let sak gpt
+i think the reason why using a fucntion work becuase its reference type, meaning
+when we do new Date that stay teh same even if we call another new data meaning
+becuase its reference type, so startTime pretty much points to the 
+new date store in the memory and so is this.timeStart and that pretty much maks it local
+to the function, and when we start another function ednTime it dose not know anotehr new Data was 
+called becayse its out of its scope but the duration now it becuase this.startTime and end Time
+is property with value set in the functoin
+*/
